45b219da19d833def969e28cea84866ef69f6329
= <9699499+yang-haoyang@user.noreply.gitee.com>
2021-11-10 13:15:59 +0800

    first commit

---
+++ app/src/main/java/com/example/stringutil/StringUtil.java
@@ -0,0 +1,19 @@
+package com.example.stringutil;
+
+import android.widget.Toast;
+
+/**
+ * Date:2021/11/10
+ * Time:13:11
+ * author:yanghaoyang
+ */
+public class StringUtil {
+    public static boolean name(String name){
+        if (name.equals("杨昊洋")&&name!=null){
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+}
